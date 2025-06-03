b06e22418ad71c0f085a32cff6051d347b09a413
Andrade_dev <joaovitorandradedealmeida@gmail.com>
2025-06-03 15:17:53 -0300

    Update segundo desafio scriptdesafio.js

--- scriptdesafio.js
+++ scriptdesafio.js
@@ -1,3 +1,4 @@
+//primeiro desafio
 function calculoDoNivelDoHeroi(){
 
   let dadosDoHeroi = "felipinho da massa";
@@ -34,3 +35,40 @@ function calculoDoNivelDoHeroi(){
      console.log(`o heroi ${dadosDoHeroi} esta no nivel ${niveis[7]}`);
     }
 }calculoDoNivelDoHeroi();
+
+
+//segundo desafio DIO.me
+ let niveis = [" Ferro", "Bronze", "Prata", "Ouro", "Diamante", "Lendário", "Imortal"];
+
+let saldoDeVitorias = calculoDasVitorias(61, 5);
+
+if (saldoDeVitorias < 10){
+     console.log(`o saldo de vitórias é: ${saldoDeVitorias} e está no nivel: ${niveis[0]}`);
+}
+else if(saldoDeVitorias >= 11 && saldoDeVitorias <= 20){
+     console.log(`o saldo de vitórias é: ${saldoDeVitorias} e está no nivel: ${niveis[1]}`);
+}
+else if(saldoDeVitorias >= 21 && saldoDeVitorias <= 50){
+     console.log(`o saldo de vitórias é: ${saldoDeVitorias} e está no nivel: ${niveis[2]}`);
+}
+else if(saldoDeVitorias >= 51 && saldoDeVitorias <= 80){
+     console.log(`o saldo de vitórias é: ${saldoDeVitorias} e está no nivel: ${niveis[3]}`);
+}
+else if(saldoDeVitorias >= 81 && saldoDeVitorias <= 90){
+     console.log(`o saldo de vitórias é: ${saldoDeVitorias} e está no nivel: ${niveis[4]}`);
+}
+else if(saldoDeVitorias >= 91 && saldoDeVitorias <= 100){
+     console.log(`o saldo de vitórias é: ${saldoDeVitorias} e está no nivel: ${niveis[5]}`);
+
+}
+else if(saldoDeVitorias >= 101){
+     console.log(`o saldo de vitórias é: ${saldoDeVitorias} e está no nivel: ${niveis[6]}`);
+}
+
+
+
+function calculoDasVitorias(vitorias, derrotas){
+    let resultado = (vitorias - derrotas);
+    return resultado;
+
+}
